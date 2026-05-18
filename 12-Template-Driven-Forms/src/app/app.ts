505e87532abdcd0afa1d5d7f2732395b43d61d5b
Banumathi Muthumariappan <banumathi.muthumariappan@clanwilliamhealth.com>
2026-05-18 11:26:57 +0530

    Project added

--- 12-Template-Driven-Forms/src/app/app.ts
+++ 12-Template-Driven-Forms/src/app/app.ts
@@ -1,27 +1,41 @@
+import { JsonPipe } from '@angular/common';
+import { JsonpInterceptor } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('12-Template-Driven-Forms');
+  skillOptions = ['Angular', 'React', 'Vue'];
+  selectedSkills: Record<string, boolean> = {
+    Angular: false,
+    React: false,
+    Vue: false
+  }
   user = {
     fullName: '',
     email: '',
     password: '',
     gender: '',
     city: '',
-    skills: [],
+    skills: [] as String[],
     about: '',
     acceptTerms: false
   }
 
+  getSelectedSkills() {
+    return this.skillOptions.filter(skill => this.selectedSkills[skill]);
+  }
+
+  
   submitForm(userForm: any) {
+    this.user.skills = this.getSelectedSkills();
     console.log(this.user);
   }
 }
